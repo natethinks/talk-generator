c7c7ba4941af2adf723c4c77d717bc121bf0efd8
Nate Meyers <natejmeyers@gmail.com>
2018-03-19 14:18:59 -0600

    successful initial request of html content on a scripture page

---
+++ talk.py
@@ -0,0 +1,12 @@
+import urllib.request
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9'
+}
+
+response = requests.get('https://www.lds.org/scriptures/bofm/1-ne/2?lang=eng', headers=headers)
+print(response.text)
